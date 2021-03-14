38f33c2f402950149b1871366c7096068159fcf5
seunhoo <seunhoo@naver.com>
2021-03-15 08:11:04 +0900

    item

--- Space/Item.h
+++ Space/Item.h
@@ -7,6 +7,12 @@ public:
 
 	int m_V;
 
+	int m_HealCount;
+	int m_RandomCount;
+	int m_InvCount;
+	int m_DefCount;
+	int m_SpeedCount;
+
 	Item(int v,float posx, float posy);
 
 	void Update(float deltatime, float time);
